9f29e5a9c5be747178aaee52d374cb5a7ba1c39d
Nabeel Boda <bodanabeel001@gmail.com>
2025-12-14 16:21:19 +0530

    auth

--- apps/next-app/src/app/note/[documentName]/page.tsx
+++ apps/next-app/src/app/note/[documentName]/page.tsx
@@ -1,19 +1,28 @@
 import { Editor } from "@/components/editor/DynamicEditor";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ documentName: string }>;
 }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   const { documentName } = await params;
-  console.log(documentName);
+  if (!session) {
+    redirect(`/auth?documentName=${documentName}`, RedirectType.replace);
+  }
   return (
     <main className="flex h-screen">
       {/* aside navbar */}
       <aside className="w-[15%] bg-[#ece7e2]/10 h-full"></aside>
 
       <section className="w-full">
-        {/* topbar */}
+        {/* topl-bar */}
         <div className="h-20 p-4 ">workspace / product / vision</div>
 
         {/* editor */}
