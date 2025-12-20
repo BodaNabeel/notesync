c55722fb3b8374c7179e04ece339f7a2c7fcc047
Nabeel Boda <bodanabeel001@gmail.com>
2025-12-20 18:19:02 +0530

    code cleanup

--- apps/hono-server/src/auth.ts
+++ apps/hono-server/src/auth.ts
@@ -7,12 +7,7 @@ const JWKS_URL = new URL(
     `${process.env.BETTER_AUTH_URL}/api/auth/jwks`
 );
 
-/**
- * jose handles:
- * - caching
- * - key rotation
- * - kid selection
- */
+
 const JWKS = createRemoteJWKSet(JWKS_URL);
 
 export async function verifyJWT(token: string): Promise<JWTPayload> {
@@ -22,10 +17,3 @@ export async function verifyJWT(token: string): Promise<JWTPayload> {
 
     return payload;
 }
-
-
-// console.log(process.env.BETTER_AUTH_URL!, "hello")
-
-// export const verifyJWT = () => {
-//     return "hi"
-// }
