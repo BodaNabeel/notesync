c2d83aa7c807d25201c50eaaccd69e328d4d4c57
Nabeel Boda <bodanabeel001@gmail.com>
2026-01-15 17:33:55 +0530

    fix: redirect to first document

--- apps/next-app/src/app/note/page.ts
+++ apps/next-app/src/app/note/page.ts
@@ -1,9 +1,30 @@
+import { db } from '@/database/drizzle';
+import { auth } from '@/lib/auth';
+import { desc, documentTable, eq } from '@note/db';
 import { randomUUID } from 'crypto';
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-    //TODO: Replace the randomUUID logic with redirecting to first note in the document
-    const uniqueDocumentName = randomUUID()
-    redirect(`note/${uniqueDocumentName}`)
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/auth")
+    }
+
+    const [document] = await db.select({ documentName: documentTable.id }).from(documentTable).where(eq(documentTable.ownerId, session.user.id)).limit(1).orderBy(
+        desc(documentTable.createdAt),
+        desc(documentTable.id)
+    )
+
+    if (document) {
+        redirect(`note/${document.documentName}`)
+    } else {
+        const uniqueDocumentName = randomUUID()
+        redirect(`note/${uniqueDocumentName}`)
+
+    }
 }
 
