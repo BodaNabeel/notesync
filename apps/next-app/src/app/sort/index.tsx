d3b902d611591bd4f6a282193ae9c74c57669ac9
Nabeel Boda <bodanabeel001@gmail.com>
2026-01-22 01:57:36 +0530

    remove unused files

--- apps/next-app/src/app/sort/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/sort/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <button onClick={() => console.log("NOT FOUND")}>NOT FOUND</button>;
-}
