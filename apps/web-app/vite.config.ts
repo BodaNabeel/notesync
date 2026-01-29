f4c901d2c1214442c22a1aafc3dd3e9dfc19a63b
Nabeel Boda <bodanabeel001@gmail.com>
2026-01-29 16:06:23 +0530

    nitro setup

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
     server: {
@@ -11,6 +12,7 @@ export default defineConfig({
     },
     plugins: [
         tailwindcss(),
+        nitro(),
         // Enables Vite to resolve imports using path aliases.
         tsconfigPaths(),
         tanstackStart({
